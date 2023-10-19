9b6b51f90c0f2fb19d0e853a64531f53cb46f73b
Dmitriy Dmitriy <dbilica19@gmail.com>
2023-10-20 00:59:06 +0300

    Add project files.

---
+++ Animals/Animals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    internal class Animals
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string View { get; set; }
+        public string Habitat { get; set; }
+        public double Area { get; set; }
+        public string Food { get; set; }
+        public string Sound { get; set; }
+
+        public void DoVoice()
+        {
+            Console.WriteLine($"{Name}, Pppp-Ppp");
+        }
+        public void Play()
+        {
+            Console.WriteLine($"{Name}, ирать!!!");
+        }
+        public void DoEat()
+        {
+            Console.WriteLine($"{Name}, кушать!");
+        }
+    }
+}
