423aea7db8856977f50a4d6fae350d908db09f9e
Dmitriy Dmitriy <dbilica19@gmail.com>
2023-10-22 16:05:57 +0300

    добавил конструктор и приватность полей

--- Animals/Animals.cs
+++ Animals/Animals.cs
@@ -6,15 +6,47 @@ using System.Threading.Tasks;
 
 namespace Animals
 {
-    internal class Animals
+    public class Animals
     {
         public string Name { get; set; }
-        public int Age { get; set; }
+        public int Weight { get; private set; }
+
+        private const int _minWeight = 30;
+        public double Age { get; private set; }
+
+        private const int _minAge = 0;
         public string View { get; set; }
         public string Habitat { get; set; }
         public double Area { get; set; }
-        public string Food { get; set; }
+        public string Food { get; private set; }
         public string Sound { get; set; }
+
+        public Animals(string name, int weihgt, double age, string view, string habitat, double area, string food, string sound) 
+        {
+            Name = name;
+            if (weihgt < _minWeight)
+            {
+                Weight = weihgt;
+            }
+            else
+            {
+                Weight = _minWeight;
+            }
+
+            if (age <= _minAge)
+            {
+                Age = age;
+            }
+            else
+            {
+                Age = _minAge;
+            }
+            View = view;
+            Habitat = habitat;
+            Area = area;
+            Food = food;
+            Sound = sound;
+        }
 
         public void DoVoice()
         {
